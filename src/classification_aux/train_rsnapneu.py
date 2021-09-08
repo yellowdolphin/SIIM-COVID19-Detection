c5a9431bd51597a764197da2bba42e122a763489
yellowdolphin <marius.wanko@gmail.com>
2021-09-08 12:48:53 +0200

    benchmark RSNAPneuAuxDataset.getitem

--- src/classification_aux/train_rsnapneu.py
+++ src/classification_aux/train_rsnapneu.py
@@ -56,15 +56,16 @@ if __name__ == "__main__":
         print("Benchmarking datasets.RSNAPneuAuxDataset.__getitem__() variants")
         from time import perf_counter
 
-        for use_reshape in (False, True):
-            for use_broadcast in (False, True):
-                valid_dataset = RSNAPneuAuxDataset(df=valid_df, images_dir='.', image_size=cfg['aux_image_size'], mode='valid',
-                                                   use_reshape=use_reshape, use_broadcast=use_broadcast)
-                tic = perf_counter()
-                for i in range(len(valid_dataset)):
-                    res = valid_dataset[i]
-                print(f"use_broadcast: {use_broadcast:5}, use_reshape: {use_reshape:5}, Wall: {perf_counter() - tic:.6f}")
-        print()
+        for n in range(10):
+            for use_reshape in (False, True):
+                for use_broadcast in (False, True):
+                    valid_dataset = RSNAPneuAuxDataset(df=valid_df, images_dir='.', image_size=cfg['aux_image_size'], mode='valid',
+                                                       use_reshape=use_reshape, use_broadcast=use_broadcast)
+                    tic = perf_counter()
+                    for i in range(len(valid_dataset)):
+                        res = valid_dataset[i]
+                    print(f"use_broadcast: {use_broadcast:5}, use_reshape: {use_reshape:5}, Wall: {perf_counter() - tic:.6f}")
+            print()
 
 
 
