aef4267cb09f5c320dbf2a7338f04e82c3c9e14d
yellowdolphin <marius.wanko@gmail.com>
2021-09-10 12:15:50 +0200

    prepare new-timm

--- src/classification_aux/segmentation_models_pytorch/encoders/__init__.py
+++ src/classification_aux/segmentation_models_pytorch/encoders/__init__.py
@@ -79,7 +79,8 @@ def get_encoder(name, in_channels=3, depth=5, weights=None):
     if not hasattr(encoder, 'make_dilated') and 'efficientnet' in name:
         # Add this method to timm models
         from functools import partial
-        
+        from torch import nn
+
         def get_stages(m):
             _stage_idxs = (2, 3, 5)
             return [
