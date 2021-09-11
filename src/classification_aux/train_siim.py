8b9ee21a7afc1c812093250000428bc7fd2e18ea
yellowdolphin <marius.wanko@gmail.com>
2021-09-11 23:33:23 +0200

    support timm act_layer kwarg

--- src/classification_aux/train_siim.py
+++ src/classification_aux/train_siim.py
@@ -89,7 +89,6 @@ if __name__ == "__main__":
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        print("get missing cfg key:", cfg['what_happens_if_key_is_missing?'])
         encoder_act_layer = args.encoder_act or cfg['encoder_act_layer'] if 'encoder_act_layer' in cfg else None
         model = SiimCovidAuxModel(
             encoder_name=cfg['encoder_name'],
