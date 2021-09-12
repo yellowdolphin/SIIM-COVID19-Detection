a5135bb39a9da4dc46ca0f7c8ec2d736390a37ad
yellowdolphin <marius.wanko@gmail.com>
2021-09-12 14:21:47 +0200

    extend dataset/loader info

--- src/classification_aux/train_siim.py
+++ src/classification_aux/train_siim.py
@@ -85,9 +85,8 @@ if __name__ == "__main__":
         valid_loader = DataLoader(valid_dataset, batch_size=batch_size, sampler=SequentialSampler(valid_dataset), 
             num_workers=cpu_count(), drop_last=False)
 
-        print("drop last valid batch:", valid_loader.drop_last)
-        print(f'TRAIN: {len(train_loader):5} batches of {batch_size} = {len(train_loader) * batch_size:8} / {len(train_loader.dataset):8} images')
-        print(f'VALID: {len(valid_loader):5} batches of {batch_size} (or less) => {len(valid_loader.dataset):8} images')
+        print(f'TRAIN: {len(train_loader):5} batches of {batch_size}     = {len(train_loader) * batch_size:6} / {len(train_loader.dataset):6} images')
+        print(f'VALID: {len(valid_loader):5} batches of {batch_size} (or less)   => {len(valid_loader.dataset):6} images')
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
